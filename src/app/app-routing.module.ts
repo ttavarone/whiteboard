0d10b4a04b7f8e2af3c44e3a34b56d316c492bb4
ttavarone <ttavarone@live.com>
2020-01-21 18:17:13 -0500

    adding changes to the app.component and the wireframe is up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'editor', component: editor }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
